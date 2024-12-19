c340ff15d6ca5a74f1b6239487548ede3d1d0cfa
vasanth-java <webdevvasanth@gmail.com>
2024-12-19 22:16:16 +0530

    all fills upload

---
+++ js/script.js
@@ -0,0 +1,204 @@
+// htmlcss progress circular bar 
+let htmlProgress = document.querySelector(".html-css"),
+  htmlValue = document.querySelector(".html-progress");
+
+let htmlStartValue = 0,
+  htmlEndValue = 90,
+  htmlspeed = 30;
+
+let progresshtml = setInterval(() => {
+  htmlStartValue++;
+
+  htmlValue.textContent = `${htmlStartValue}%`;
+  htmlProgress.style.background = `conic-gradient(#fca61f ${
+    htmlStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (htmlStartValue == htmlEndValue) {
+    clearInterval(progresshtml);
+  }
+}, htmlspeed);
+
+
+// Angular Progress Bar
+let angularProgress = document.querySelector(".angular"),
+  angularValue = document.querySelector(".angular-progress");
+
+let angularStartValue = 0,
+  angularEndValue = 75,
+  angularSpeed = 30;
+
+let progressangular = setInterval(() => {
+  angularStartValue++;
+
+  angularValue.textContent = `${angularStartValue}%`;
+  angularProgress.style.background = `conic-gradient(#0573f7 ${angularStartValue * 3.6}deg, #ededed 0deg)`;
+
+  if (angularStartValue === angularEndValue) {
+    clearInterval(progressangular);
+  }
+}, angularSpeed);
+
+
+// Spring Boot Progress Bar
+let springProgress = document.querySelector(".spring"),
+  springValue = document.querySelector(".spring-progress");
+
+let springStartValue = 0,
+  springEndValue = 60,
+  springSpeed = 30;
+
+let progressspring = setInterval(() => {
+  springStartValue++;
+
+  springValue.textContent = `${springStartValue}%`;
+  springProgress.style.background = `conic-gradient(#10ff17 ${springStartValue * 3.6}deg, #ededed 0deg)`;
+
+  if (springStartValue === springEndValue) {
+    clearInterval(progressspring);
+  }
+}, springSpeed);
+
+
+// MySQL Progress Bar
+let mysqlProgress = document.querySelector(".mysql"),
+  mysqlValue = document.querySelector(".mysql-progress");
+
+let mysqlStartValue = 0,
+  mysqlEndValue = 85,
+  mysqlSpeed = 30;
+
+let progressmysql = setInterval(() => {
+  mysqlStartValue++;
+
+  mysqlValue.textContent = `${mysqlStartValue}%`;
+  mysqlProgress.style.background = `conic-gradient(#ba00f6 ${mysqlStartValue * 3.6}deg, #ededed 0deg)`;
+
+  if (mysqlStartValue === mysqlEndValue) {
+    clearInterval(progressmysql);
+  }
+}, mysqlSpeed);
+
+// javasript progress circular bar 
+let javascriptProgress = document.querySelector(".javascript"),
+  javascriptValue = document.querySelector(".javascript-progress");
+
+let javascriptStartValue = 0,
+  javascriptEndValue = 80,
+  jsspeed = 30;
+
+let progressjs = setInterval(() => {
+  javascriptStartValue++;
+
+  javascriptValue.textContent = `${javascriptStartValue}%`;
+  javascriptProgress.style.background = `conic-gradient(#7d2ae8 ${
+    javascriptStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (javascriptStartValue == javascriptEndValue) {
+    clearInterval(progressjs);
+  }
+}, jsspeed);
+
+// php progress circular bar 
+let phpProgress = document.querySelector(".php"),
+  phpValue = document.querySelector(".php-progress");
+
+let phpStartValue = 0,
+  phpEndValue = 85,
+  phpspeed = 30;
+
+let progressphp = setInterval(() => {
+  phpStartValue++;
+
+  phpValue.textContent = `${phpStartValue}%`;
+  phpProgress.style.background = `conic-gradient(#20c997 ${
+    phpStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (phpStartValue == phpEndValue) {
+    clearInterval(progressphp);
+  }
+}, phpspeed);
+
+// reactjs progress circular bar 
+let reactProgress = document.querySelector(".reactjs"),
+  reactValue = document.querySelector(".reactjs-progress");
+
+let reactStartValue = 0,
+  reactEndValue = 70,
+  rjsspeed = 30;
+
+let progressreact = setInterval(() => {
+  reactStartValue++;
+
+  reactValue.textContent = `${reactStartValue}%`;
+  reactProgress.style.background = `conic-gradient(#3f396d ${
+    reactStartValue * 3.6
+  }deg, #ededed 0deg)`;
+
+  if (reactStartValue == reactEndValue) {
+    clearInterval(progressreact);
+  }
+}, rjsspeed);
+
+
+// filter using javascript
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value == "all") {
+      $(".post").show("1000");
+    } else {
+      $(".post")
+        .not("." + value)
+        .hide("1000");
+      $(".post")
+        .filter("." + value)
+        .show("1000");
+    }
+  });
+});
+
+
+// javascript for sticky navbar even if u scroll the navbar will be fixed
+document.addEventListener("DOMContentLoaded", function(){
+  window.addEventListener('scroll', function() {
+      if (window.scrollY > 50) {
+        document.getElementById('navbar-top').classList.add('fixed-top');
+        // add padding top to show content behind navbar
+        navbar_height = document.querySelector('.navbar').offsetHeight;
+        document.body.style.paddingTop = navbar_height + 'px';
+      } else {
+        document.getElementById('navbar-top').classList.remove('fixed-top');
+         // remove padding top from body
+        document.body.style.paddingTop = '0';
+      } 
+  });
+}); 
+
+
+// adding funtionality to back to top button 
+
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click",function(){
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
